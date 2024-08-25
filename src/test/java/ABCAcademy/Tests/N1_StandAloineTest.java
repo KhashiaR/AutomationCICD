f0269c994ffb9b78aa3d221684ba3f062a22f12a
KhashiaR <khashiarehman1@gmail.com>
2024-08-25 20:33:47 +0500

    updated stand alone

--- src/test/java/ABCAcademy/Tests/N1_StandAloineTest.java
+++ src/test/java/ABCAcademy/Tests/N1_StandAloineTest.java
@@ -24,7 +24,7 @@ public class N1_StandAloineTest {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		//new comments are added
+		//new comments are added jaaaa
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
